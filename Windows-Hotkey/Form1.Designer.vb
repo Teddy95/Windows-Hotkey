0af97860640146d2bf4bde85a97997d18d311aa0
Andre Sieverding <webmaster@andre-sieverding.de>
2019-03-06 13:33:50 +0100

    Update

--- Windows-Hotkey/Form1.Designer.vb
+++ Windows-Hotkey/Form1.Designer.vb
@@ -25,11 +25,21 @@ Partial Class Form1
         Me.Button1 = New System.Windows.Forms.Button()
         Me.Button2 = New System.Windows.Forms.Button()
         Me.BackgroundWorker1 = New System.ComponentModel.BackgroundWorker()
+        Me.RadioButton1 = New System.Windows.Forms.RadioButton()
+        Me.RadioButton2 = New System.Windows.Forms.RadioButton()
+        Me.RadioButton3 = New System.Windows.Forms.RadioButton()
+        Me.RadioButton4 = New System.Windows.Forms.RadioButton()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.ComboBox1 = New System.Windows.Forms.ComboBox()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.NumericUpDown1 = New System.Windows.Forms.NumericUpDown()
+        CType(Me.NumericUpDown1, System.ComponentModel.ISupportInitialize).BeginInit()
         Me.SuspendLayout()
         '
         'Button1
         '
-        Me.Button1.Location = New System.Drawing.Point(12, 12)
+        Me.Button1.Location = New System.Drawing.Point(131, 120)
         Me.Button1.Name = "Button1"
         Me.Button1.Size = New System.Drawing.Size(75, 23)
         Me.Button1.TabIndex = 0
@@ -39,7 +49,7 @@ Partial Class Form1
         'Button2
         '
         Me.Button2.Enabled = False
-        Me.Button2.Location = New System.Drawing.Point(116, 12)
+        Me.Button2.Location = New System.Drawing.Point(212, 120)
         Me.Button2.Name = "Button2"
         Me.Button2.Size = New System.Drawing.Size(75, 23)
         Me.Button2.TabIndex = 1
@@ -49,11 +59,107 @@ Partial Class Form1
         'BackgroundWorker1
         '
         '
+        'RadioButton1
+        '
+        Me.RadioButton1.AutoSize = True
+        Me.RadioButton1.Location = New System.Drawing.Point(15, 34)
+        Me.RadioButton1.Name = "RadioButton1"
+        Me.RadioButton1.Size = New System.Drawing.Size(52, 17)
+        Me.RadioButton1.TabIndex = 2
+        Me.RadioButton1.Text = "Keine"
+        Me.RadioButton1.UseVisualStyleBackColor = True
+        '
+        'RadioButton2
+        '
+        Me.RadioButton2.AutoSize = True
+        Me.RadioButton2.Checked = True
+        Me.RadioButton2.Location = New System.Drawing.Point(15, 57)
+        Me.RadioButton2.Name = "RadioButton2"
+        Me.RadioButton2.Size = New System.Drawing.Size(101, 17)
+        Me.RadioButton2.TabIndex = 3
+        Me.RadioButton2.TabStop = True
+        Me.RadioButton2.Text = "Shift / Umschalt"
+        Me.RadioButton2.UseVisualStyleBackColor = True
+        '
+        'RadioButton3
+        '
+        Me.RadioButton3.AutoSize = True
+        Me.RadioButton3.Location = New System.Drawing.Point(15, 80)
+        Me.RadioButton3.Name = "RadioButton3"
+        Me.RadioButton3.Size = New System.Drawing.Size(70, 17)
+        Me.RadioButton3.TabIndex = 4
+        Me.RadioButton3.Text = "Crtl / Strg"
+        Me.RadioButton3.UseVisualStyleBackColor = True
+        '
+        'RadioButton4
+        '
+        Me.RadioButton4.AutoSize = True
+        Me.RadioButton4.Location = New System.Drawing.Point(15, 103)
+        Me.RadioButton4.Name = "RadioButton4"
+        Me.RadioButton4.Size = New System.Drawing.Size(37, 17)
+        Me.RadioButton4.TabIndex = 5
+        Me.RadioButton4.Text = "Alt"
+        Me.RadioButton4.UseVisualStyleBackColor = True
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(12, 9)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(96, 13)
+        Me.Label1.TabIndex = 6
+        Me.Label1.Text = "Kombinationstaste:"
+        '
+        'ComboBox1
+        '
+        Me.ComboBox1.FormattingEnabled = True
+        Me.ComboBox1.Items.AddRange(New Object() {"BACKSPACE", "DELETE", "ENTER", "ESC", "HOME", "INSERT", "TAB", "F1", "F2", "F3", "F4", "F5", "F6", "F7", "F8", "F9", "F10", "F11", "F12", "F13", "F14", "F15", "F16"})
+        Me.ComboBox1.Location = New System.Drawing.Point(131, 33)
+        Me.ComboBox1.Name = "ComboBox1"
+        Me.ComboBox1.Size = New System.Drawing.Size(156, 21)
+        Me.ComboBox1.TabIndex = 7
+        Me.ComboBox1.Text = "F9"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(128, 9)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(62, 13)
+        Me.Label2.TabIndex = 8
+        Me.Label2.Text = "Haupttaste:"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(128, 65)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(125, 13)
+        Me.Label3.TabIndex = 9
+        Me.Label3.Text = "Zeitinterval in Sekunden:"
+        '
+        'NumericUpDown1
+        '
+        Me.NumericUpDown1.Location = New System.Drawing.Point(131, 86)
+        Me.NumericUpDown1.Name = "NumericUpDown1"
+        Me.NumericUpDown1.Size = New System.Drawing.Size(156, 20)
+        Me.NumericUpDown1.TabIndex = 10
+        Me.NumericUpDown1.Value = New Decimal(New Integer() {30, 0, 0, 0})
+        '
         'Form1
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(203, 46)
+        Me.ClientSize = New System.Drawing.Size(299, 155)
+        Me.Controls.Add(Me.NumericUpDown1)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.ComboBox1)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.RadioButton4)
+        Me.Controls.Add(Me.RadioButton3)
+        Me.Controls.Add(Me.RadioButton2)
+        Me.Controls.Add(Me.RadioButton1)
         Me.Controls.Add(Me.Button2)
         Me.Controls.Add(Me.Button1)
         Me.MaximizeBox = False
@@ -61,11 +167,22 @@ Partial Class Form1
         Me.Name = "Form1"
         Me.ShowIcon = False
         Me.Text = "Windows HotKey"
+        CType(Me.NumericUpDown1, System.ComponentModel.ISupportInitialize).EndInit()
         Me.ResumeLayout(False)
+        Me.PerformLayout()
 
     End Sub
 
     Friend WithEvents Button1 As Button
     Friend WithEvents Button2 As Button
     Friend WithEvents BackgroundWorker1 As System.ComponentModel.BackgroundWorker
+    Friend WithEvents RadioButton1 As RadioButton
+    Friend WithEvents RadioButton2 As RadioButton
+    Friend WithEvents RadioButton3 As RadioButton
+    Friend WithEvents RadioButton4 As RadioButton
+    Friend WithEvents Label1 As Label
+    Friend WithEvents ComboBox1 As ComboBox
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents NumericUpDown1 As NumericUpDown
 End Class
--- Windows-Hotkey/Form1.vb
+++ Windows-Hotkey/Form1.vb
@@ -2,17 +2,73 @@
 
 Public Class Form1
     Dim send = False
+    Dim interval = 5000 ' Standardmäßig 30 Sekunden
+    Dim extraKey, mainKey, key As String
 
     Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        ' send auf true setzen
         send = True
+
+        ' Steuerelemente deaktivieren
         Button1.Enabled = False
+        RadioButton1.Enabled = False
+        RadioButton2.Enabled = False
+        RadioButton3.Enabled = False
+        RadioButton4.Enabled = False
+        ComboBox1.Enabled = False
+        NumericUpDown1.Enabled = False
+
+        ' Stop-Button aktivieren
         Button2.Enabled = True
+
+        ' Interval Eingabe prüfen und in Millisekunden umrechnen
+        If NumericUpDown1.Value > 0 Then
+            interval = NumericUpDown1.Value * 1000
+        End If
+
+        ' Zusatztaste festlegen
+        If RadioButton1.Checked Then
+            extraKey = ""
+        End If
+        If RadioButton2.Checked Then
+            extraKey = "+"
+        End If
+        If RadioButton3.Checked Then
+            extraKey = "^"
+        End If
+        If RadioButton4.Checked Then
+            extraKey = "%"
+        End If
+
+        ' Haupttaste ermitteln
+        If ComboBox1.SelectedItem <> "" Then
+            mainKey = ComboBox1.SelectedItem
+        Else
+            MsgBox("Bitte Taste zum simulieren angeben!")
+            Button2.PerformClick()
+            Exit Sub
+        End If
+
+        ' SendKey String zusammenstellen
+        key = extraKey + "{" + mainKey + "}"
+
         BackgroundWorker1.RunWorkerAsync()
     End Sub
 
     Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        ' send auf false setzen
         send = False
+
+        ' Steuerelemente aktivieren
         Button1.Enabled = True
+        RadioButton1.Enabled = True
+        RadioButton2.Enabled = True
+        RadioButton3.Enabled = True
+        RadioButton4.Enabled = True
+        ComboBox1.Enabled = True
+        NumericUpDown1.Enabled = True
+
+        ' Stop-Button deaktivieren
         Button2.Enabled = False
     End Sub
 
@@ -20,8 +76,8 @@ Public Class Form1
         System.Threading.Thread.Sleep(10000) ' 10 Sekunden warten, bevor der erste Tastenbefehl simuliert wird
 
         While send
-            SendKeys.SendWait("{TAB}")
-            System.Threading.Thread.Sleep(30000) ' Alle 30 Sekunden Taste simulieren
+            SendKeys.SendWait(key)
+            System.Threading.Thread.Sleep(interval) ' Taste simulieren im festgelegten Zeitinterval
         End While
     End Sub
 End Class
